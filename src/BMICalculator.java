8943e14d09baf251c835e07b69bc9a819ca7b936
Thang Tien_21020403 <cao.hl.thang@gmail.com>
2023-09-20 21:44:17 +0700

    First commit

---
+++ src/BMICalculator.java
@@ -0,0 +1,19 @@
+public class BMICalculator {
+    public static String calculateBMI(double weight, double height) {
+        if (weight <= 0 || weight >= 600) return "Cân nặng không hợp lệ";
+        if (height <= 0 || height >= 3) return "Chiều cao không hợp lệ";
+        double BMI = weight / height / height;
+        BMI = Math.round(BMI * 10) / 10.0;
+        if (BMI < 18.5) {
+            return "Thiếu cân";
+        }
+        if (BMI < 23) {
+            return "Bình thường";
+        }
+        if (BMI < 25) {
+            return "Thừa cân";
+        }
+        return "Béo phì";
+    }
+
+}
